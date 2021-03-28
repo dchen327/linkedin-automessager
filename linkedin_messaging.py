bbae50aa3af829df15061b5a941d492c1e1edc15
David Chen <davidc3287@gmail.com>
2021-03-28 16:28:07 -0400

    finalize testing

--- linkedin_messaging.py
+++ linkedin_messaging.py
@@ -1,5 +1,54 @@
 import pyautogui
-import pyautogui._pyautogui_win as platformModule
-import pyperclip
+import platform
+from time import sleep
 
-print('hi')
+cmd = 'cmd' if platform.system() == 'Darwin' else 'ctrl'
+message = '''
+hey!!
+- testing linkedin messaging script
+- bye :)
+'''.strip()
+
+
+def alt_tab():
+    pyautogui.keyDown('alt')
+    sleep(0.3)
+    pyautogui.press('tab')
+    sleep(0.3)
+    pyautogui.keyUp('alt')
+
+
+def hit_tab(num=1):
+    for _ in range(num):
+        pyautogui.hotkey('tab')
+        sleep(0.2)
+
+
+def load_links():
+    with open('profile_links.txt') as f:
+        return f.read().splitlines()
+
+
+if __name__ == '__main__':
+    links = load_links()
+    alt_tab()
+    for link in links:
+        sleep(0.5)
+        pyautogui.hotkey(cmd, 'l')  # go to address bar
+        sleep(0.5)
+        pyautogui.typewrite(link)
+        sleep(0.3)
+        pyautogui.hotkey('enter')  # load page
+        sleep(4)  # wait for page load
+        pyautogui.click(50, 300)
+        hit_tab(2)
+        pyautogui.hotkey('enter')  # hit connect button
+        sleep(0.5)
+        hit_tab(2)
+        pyautogui.hotkey('enter')  # hit add note button
+        sleep(0.5)
+        pyautogui.typewrite(message)
+        sleep(0.2)
+        hit_tab(3)
+        pyautogui.hotkey('enter')  # send message and connect
+        sleep(1.5)
